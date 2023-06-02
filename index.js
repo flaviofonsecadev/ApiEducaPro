c90bdce780e69bf990b96a8d10ec47f86865e919
flaviofonsecadev <flaviofonsecadev@gmail.com>
2023-06-01 21:25:00 -0300

    config GET docs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 
 const PDF_FOLDER = path.join(__dirname, 'pdfs');
+const DOC_FOLDER = path.join(__dirname, 'docs')
 const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // Configuração do CORS
@@ -11,6 +12,13 @@ app.use(cors()); // Configuração do CORS
 app.get('/pdfs/:filename', (req, res) => {
   const { filename } = req.params;
   const filePath = path.join(PDF_FOLDER, filename);
+
+  res.sendFile(filePath);
+});
+
+app.get('/docs/:filename', (req, res) => {
+  const { filename } = req.params;
+  const filePath = path.join(DOC_FOLDER, filename);
 
   res.sendFile(filePath);
 });
